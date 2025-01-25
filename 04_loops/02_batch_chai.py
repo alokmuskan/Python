25253d84c23f83f0eefedd494cd814235c3a63e7
alok raj <alokraj1319@gmail.com>
2025-01-25 14:48:01 +0530

    keywords can't be used as variables

--- 04_loops/02_batch_chai.py
+++ 04_loops/02_batch_chai.py
@@ -1,2 +1,6 @@
 for batch in range(1,5):  # last number is always exclusive in python
     print(f"Preparing chai for batch #{batch}")
+
+# i is a varibale here and we can use any other variable instead a/c to our choice but have to some keywords
+for i in range(1,5):  
+    print(f"Preparing chai for batch #{batch}")
