0fc6de9fddf9170355d3351b21e98f7497fec3e9
alok raj <alokraj1319@gmail.com>
2025-12-01 22:19:57 +0530

    set comprehensions important example

--- 07_comprehensions/02_set_comprehension.py
+++ 07_comprehensions/02_set_comprehension.py
@@ -1,7 +1,17 @@
-favourite_chais = [
-    "masala chai", "green tea", "masala chai",
-    "lemon tea", "green tea", "elaichi chai"
-]
+# favourite_chais = [
+#     "masala chai", "green tea", "masala chai",
+#     "lemon tea", "green tea", "elaichi chai"
+# ]
 
-unique_chai = {chai for chai in favourite_chais}
-print(unique_chai)
+# unique_chai = {chai for chai in favourite_chais}
+# print(unique_chai)
+
+
+recipes = {
+    "masala chai": ["ginger", "cardamom", "clove"],
+    "elaichi chai": ["cardamom", "milk"],
+    "spicy chai": ["ginger", "black pepper", "clove"],
+}
+
+unique_spices = {spice for ingredients in recipes.values() for spice in ingredients}
+print(unique_spices)
