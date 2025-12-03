d3dfebad2180445c254c74841b2d0a18bff7c3fa
alok raj <alokraj1319@gmail.com>
2025-12-03 22:03:08 +0530

    set comprehensions works here without if condition also

---
+++ 07_comprehensions/02_set_comprehension.py
@@ -0,0 +1,7 @@
+favourite_chais = [
+    "masala chai", "green tea", "masala chai",
+    "lemon tea", "green tea", "elaichi chai"
+]
+
+unique_chai = {chai for chai in favourite_chais}
+print(unique_chai)
