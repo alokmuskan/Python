916759f561722045d48a267e681a155544d9e0bf
alok raj <alokraj1319@gmail.com>
2025-12-04 12:20:28 +0530

    comprehensions -> concise way of writing code

---
+++ 07_comprehensions/01_list_comprehension.py
@@ -0,0 +1,10 @@
+menu = [
+    "masala Chai",
+    "Iced lemon tea",
+    "ginger tea",
+    "Iced peach tea",
+    "Green chai"
+]
+
+iced_tea = [tea for tea in menu if "Iced" in tea]
+print(iced_tea)
