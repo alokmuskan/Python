591a7d744d75b958235441cb9c694c8e30ebe9c4
alok raj <alokraj1319@gmail.com>
2025-12-05 11:49:00 +0530

    third way of importing, we can directly use dot or dots for importing just we need to see the levels

--- 06_chai_business/main.py
+++ 06_chai_business/main.py
@@ -2,6 +2,11 @@
 
 # print(recipes.flavors.ginger_chai())
 
-from recipes.flavors import elaichi_chai, ginger_chai  # named import   # press ctrl + space for suggestions
 
-print(ginger_chai())
+
+# from recipes.flavors import elaichi_chai, ginger_chai  # named import   # press ctrl + space for suggestions
+
+# print(ginger_chai())
+
+
+from .recipes.flavors import ginger_chai  # .. the two dots take us to the root directory so can use one dot if they are on the same level 
