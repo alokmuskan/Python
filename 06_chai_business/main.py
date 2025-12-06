5c7cbbb4d7780776228232c9478c4221f6eeca28
alok raj <alokraj1319@gmail.com>
2025-12-06 11:44:41 +0530

    named import -> another way of importing and here we use ctrl + space for suggesations

--- 06_chai_business/main.py
+++ 06_chai_business/main.py
@@ -1,3 +1,7 @@
-import recipes.flavors
+# import recipes.flavors
 
-print(recipes.flavors.ginger_chai())
+# print(recipes.flavors.ginger_chai())
+
+from recipes.flavors import elaichi_chai, ginger_chai  # named import   # press ctrl + space for suggestions
+
+print(ginger_chai())
