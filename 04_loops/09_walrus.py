3239f99a653cf32d2f2acae044f1683974557b22
alok raj <alokraj1319@gmail.com>
2025-12-24 00:07:08 +0530

    learnt the basics of dictionary

--- 04_loops/09_walrus.py
+++ 04_loops/09_walrus.py
@@ -5,19 +5,20 @@
 #     print(f"Not divisible, remainder is {remainder}")
 
 
-value = 13
+# value = 13
 
-if (remainder := value % 5):
-    print(f"Not divisible, remainder is {remainder}")
+# if (remainder := value % 5):
+#     print(f"Not divisible, remainder is {remainder}")
 
 
 
-available_size = ["small", "medium", "large"]
+# available_size = ["small", "medium", "large"]
 
-if(requested_size := input("Entre your chai cup size: ")) in available_size:
-    print(f"Serving {requested_size} chai")
-else:
-    print(f"Size is unavaiable - {requested_size}")
+# if(requested_size := input("Entre your chai cup size: ")) in available_size:
+#     print(f"Serving {requested_size} chai")
+# else:
+#     print(f"Size is unavaiable - {requested_size}")
+
 
 
 
@@ -27,4 +28,5 @@ print(f"Available flavours: ", flavours)
 
 while (flavour := input("Choose your flavour: ")) not in flavours:
     print(f"Sorry, {flavour} is not available")
+    
 print(f"You Choose {flavour} chai")  # this line is not part of loop
