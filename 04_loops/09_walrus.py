f731cce503bf65a81a7666ae1cec3db6ed132ae7
alok raj <alokraj1319@gmail.com>
2025-12-30 23:08:33 +0530

    another application of walrus operator

--- 04_loops/09_walrus.py
+++ 04_loops/09_walrus.py
@@ -9,3 +9,12 @@ value = 13
 
 if (remainder := value % 5):
     print(f"Not divisible, remainder is {remainder}")
+
+
+
+available_size = ["small", "medium", "large"]
+
+if(requested_size := input("Entre your chai cup size: ")) in available_size:
+    print(f"Serving {requested_size} chai")
+else:
+    print(f"Size is unavaiable - {requested_size}")
