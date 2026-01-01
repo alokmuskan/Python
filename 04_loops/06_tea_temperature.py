fc3bfb89258b27577d62c48d91ac6df7518377b8
alok raj <alokraj1319@gmail.com>
2026-01-01 19:41:00 +0530

    solved a problem of while loop

--- 04_loops/06_tea_temperature.py
+++ 04_loops/06_tea_temperature.py
@@ -2,7 +2,7 @@ temperature = 40
 
 while temperature < 100:
     print(f"Current Temperature : {temperature}")
-    # temperature = temperature = 15
-    temperature =+ 15
+    # temperature = temperature = 15  
+    temperature =+ 15      # short hand for performing this addition operation
 
 print(f"Tea is ready to boil.")
