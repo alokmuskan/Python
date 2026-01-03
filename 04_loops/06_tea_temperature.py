eb5e0bab39a7d592dc17b430d8cc872752b3ce5f
alok raj <alokraj1319@gmail.com>
2026-01-03 20:02:05 +0530

    corrected the shorthand notation

--- 04_loops/06_tea_temperature.py
+++ 04_loops/06_tea_temperature.py
@@ -3,6 +3,6 @@ temperature = 40
 while temperature < 100:
     print(f"Current Temperature : {temperature}")
     # temperature = temperature = 15  
-    temperature =+ 15      # short hand for performing this addition operation
+    temperature += 15      # short hand for performing this addition operation
 
 print(f"Tea is ready to boil.")
