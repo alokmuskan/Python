77c17df6e7cddca5662f925fdfc76078cb9e7720
alok raj <alokraj1319@gmail.com>
2026-01-08 22:57:30 +0530

    learnt about walrus operator and written the code again using walrus operator

---
+++ 04_loops/09_walrus.py
@@ -0,0 +1,11 @@
+# value = 13
+# remainder = value % 5
+
+# if remainder:
+#     print(f"Not divisible, remainder is {remainder}")
+
+
+value = 13
+
+if (remainder := value % 5):
+    print(f"Not divisible, remainder is {remainder}")
