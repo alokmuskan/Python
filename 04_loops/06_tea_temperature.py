2f1987a1d4bd859e6ef0309556bdeee20c93d240
alok raj <alokraj1319@gmail.com>
2026-01-23 15:21:48 +0530

    used shorthand for performing operations

--- 04_loops/06_tea_temperature.py
+++ 04_loops/06_tea_temperature.py
@@ -2,6 +2,7 @@ temperature = 40
 
 while temperature < 100:
     print(f"Current Temperature : {temperature}")
-    temperature = temperature + 15
+    # temperature = temperature = 15
+    temperature =+ 15
 
 print(f"Tea is ready to boil.")
