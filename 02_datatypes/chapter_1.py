b8b7425b55983418798dd23d6242041ed428be19
alok raj <alokraj1319@gmail.com>
2026-02-09 23:04:05 +0530

    Numbers are immutable and their immutability should be tracked by Identity not by values

---
+++ 02_datatypes/chapter_1.py
@@ -0,0 +1,10 @@
+sugar_amount = 2
+print(f"Initial Sugar : {sugar_amount}")
+
+sugar_amount = 12
+print(f"Initial Sugar : {sugar_amount}")
+
+#Numbers are Immutable then how it changes remember always to track with the identity not by value
+
+print(f"Id of 2 : {id(2)}")     #Both of them have different Id so they are Immutable 
+print(f"Id of 12 : {id(12)}")
