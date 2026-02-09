1f326fd47fb026579ed946693ff640033a4da9d2
alok raj <alokraj1319@gmail.com>
2026-02-09 23:47:13 +0530

    these are the basic operation that we can perform in Pythom

---
+++ 02_datatypes/chapter_3.py
@@ -0,0 +1,16 @@
+# Integer
+
+black_tea_grams = 14
+ginger_grams = 3
+
+total_grams = black_tea_grams + ginger_grams
+print(f"Total grams of base tea is {total_grams}")
+
+remaining_tea = black_tea_grams - ginger_grams
+print(f"Total grams of remaining tea is {total_grams}")
+
+# / -> Normal division
+# // -> Don't care what comes after decimal point , it gives thefloor value
+# * -> Multiplication
+# ** -> Power 
+# billion = 10_000_000 accepted in python this ensures that readability is how much important in Python
