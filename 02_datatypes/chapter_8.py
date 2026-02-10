8f15cc5420ac9e811d3cfc7a42f8406867759531
alok raj <alokraj1319@gmail.com>
2026-02-10 21:53:38 +0530

    when an operator is used to perform more than one work then it's operator overloading

--- 02_datatypes/chapter_8.py
+++ 02_datatypes/chapter_8.py
@@ -15,3 +15,15 @@ chai_ingredients.extend(spice_options)
 print(f"Chai : {chai_ingredients}")
 chai_ingredients.insert(2, "black tea")
 print(f"Chai : {chai_ingredients}")
+
+
+# operator overloading  -> when an operator is used to perform more than one work
+
+base_liquid = ["watre" , "milk"]
+extra_flavour = ["ginger"]
+
+full_liquid_mix = base_liquid + extra_flavour
+print(f"Liquid mix : {full_liquid_mix}")  # works same as string concatination
+
+strong_brew = ["black tea", "milk"] * 3   # maintains the order and prints three times
+print(f"Strong brew : {strong_brew}")
