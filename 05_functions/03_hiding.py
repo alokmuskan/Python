e87bddeca88dd57fa7bfdf3b78664e30657657c1
alok raj <alokraj1319@gmail.com>
2026-02-13 22:36:34 +0530

    solved another question of function

---
+++ 05_functions/03_hiding.py
@@ -0,0 +1,14 @@
+def get_input():
+    print("Getting user input")
+
+def validate_input():
+    print("Validate the user input")
+
+def save_to_db():
+    print("saving to database")
+
+def registerUser():
+    get_input()
+    validate_input()
+    save_to_db()
+    print("User registration complete")
