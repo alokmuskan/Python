1b7b3a7a79cf63ebbf54e250a7bfe5a460f82087
alok raj <alokraj1319@gmail.com>
2026-03-01 16:35:43 +0530

    way of writing documentation

--- 05_functions/12_built_in.py
+++ 05_functions/12_built_in.py
@@ -8,3 +8,15 @@ print(chai_flavour.__doc__)
 print(chai_flavour.__name__)
 
 help(len)  # another builtin function to which we can pass anything 
+
+
+def generate_bill(chai=0, samosa=0):
+    """
+    Calculate the total bill for chai and samosa 
+    :param chai: Number of chai cups (10 rupees each)
+    :param samosa: Number of samosa (15 repees each)
+    : return: (total amount, thank you message as a string )
+    """
+
+    total = chai*10 + samosa*15
+    return total, "Thank you for visiting chaicode.com"
