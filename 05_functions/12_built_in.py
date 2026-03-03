efb50f7b776b977532b257b895b9444c8d79d4d1
alok raj <alokraj1319@gmail.com>
2026-03-03 16:24:31 +0530

    these are builtin of python

---
+++ 05_functions/12_built_in.py
@@ -0,0 +1,8 @@
+def chai_flavour(flavour="masala"):
+    """Return the flavour of chai."""   # this must be at the very first line 
+    chai="ginger"
+    return flavour
+
+
+print(chai_flavour.__doc__)
+print(chai_flavour.__name__)
