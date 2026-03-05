96e6fc0ee3aad8310527b0ab230e29c3c1b76bd7
alok raj <alokraj1319@gmail.com>
2026-03-05 20:58:48 +0530

    recursive function

--- 05_functions/11_types_of_functions.py
+++ 05_functions/11_types_of_functions.py
@@ -6,3 +6,11 @@ total_chai = 0
 def impure(cups):     # not recommended 
     global total_chai
     total_chai += cups
+
+
+def pour_chai(n):     # recursive function 
+    print(n)
+    if n==0:
+        return "All chai is poured"
+    return pour_chai(n-1)
+print(pour_chai(3))
