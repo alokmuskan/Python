491b93b53771b2f74f3da13b343a9c6990e53800
alok raj <alokraj1319@gmail.com>
2026-03-09 21:00:14 +0530

    value is changed that means lists are mutable

--- 05_functions/09_input_params.py
+++ 05_functions/09_input_params.py
@@ -1,6 +1,14 @@
-chai = "Ginger chai"
+# chai = "Ginger chai"
 
-def prepare_chai(order):
-    print("Preparing ", order)
+# def prepare_chai(order):
+#     print("Preparing ", order)
 
-prepare_chai(chai)
+# prepare_chai(chai)
+
+
+chai = [1,2,3]
+def edit_chai(cup):
+    cup[1] = 42
+
+edit_chai(chai)
+print(chai)
