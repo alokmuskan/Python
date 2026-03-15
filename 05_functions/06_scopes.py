9fc20de85cdafcb41d41c649b03852cf18224737
alok raj <alokraj1319@gmail.com>
2026-03-15 20:01:19 +0530

    outside function can't access the variable inside a particular function

--- 05_functions/06_scopes.py
+++ 05_functions/06_scopes.py
@@ -1,3 +1,8 @@
 def serve_chai():
     chai_type = "Masala"     # local scope -> means the validation of this variable is just inside this function
     print(f"Chai type is {chai_type}")
+
+
+chai_type = "Lemon"
+serve_chai()
+print(f"Outside Function: {chai_type}")
