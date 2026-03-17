112e1edb9de4abc0eb8e8382b581a5cf06f71cd1
alok raj <alokraj1319@gmail.com>
2026-03-17 23:01:23 +0530

    default traps

--- 05_functions/09_input_params.py
+++ 05_functions/09_input_params.py
@@ -20,7 +20,14 @@
 # make_chai(tea="green", sugar="no", milk="little") #keywords or kargs
 
 
-def special_chai(*ingredients, **extras):
-    print("Ingredients", ingredients)
-    print("Extras", extras)
-special_chai("cinnamon","cardmom", sweetener="Honey", foam="yes")
+# def special_chai(*ingredients, **extras):
+#     print("Ingredients", ingredients)
+#     print("Extras", extras)
+# special_chai("cinnamon","cardmom", sweetener="Honey", foam="yes")
+
+def chai_order(order=[]):
+    order.append("Masala")
+    print(order)
+
+chai_order()
+chai_order()
