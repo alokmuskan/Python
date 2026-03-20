9e3860989623dc39ade1066d534fdeac7ff43e19
alok raj <alokraj1319@gmail.com>
2026-03-20 22:45:16 +0530

    this is called keyword or kargs

--- 05_functions/09_input_params.py
+++ 05_functions/09_input_params.py
@@ -6,15 +6,15 @@
 # prepare_chai(chai)
 
 
-chai = [1,2,3]
-def edit_chai(cup):
-    cup[1] = 42
+# chai = [1,2,3]
+# def edit_chai(cup):
+#     cup[1] = 42
 
-edit_chai(chai)
-print(chai)
+# edit_chai(chai)
+# print(chai)
 
 
 def make_chai(tea, milk, sugar):
     print(tea, milk, sugar)
-
 make_chai("darjeeling", "yes", "sugar") #positional
+make_chai(tea="green", sugar="no", milk="little") #keywords or kargs
